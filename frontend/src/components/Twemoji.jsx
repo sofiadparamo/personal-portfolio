99adfbf266f0ca425d82b5caf9850b061d3e714b
SweetSofiMC <magallon.paramo@gmail.com>
2022-01-07 00:43:19 -0600

    Add Twemoji component

---
+++ frontend/src/components/Twemoji.jsx
@@ -0,0 +1,19 @@
+import {Image} from "react-bootstrap";
+
+let Twemoji = (props) => {
+    return(
+        <Image
+            src={`https://twemoji.maxcdn.com/v/latest/svg/${props.emoji.codePointAt(0).toString(16)}.svg`}
+            height={props.size}
+            width={props.size}
+            alt={props.emoji}
+            style={{marginTop: "-5%"}}
+        />
+    )
+}
+
+Twemoji.defaultProps = {
+    size: 72,
+};
+
+export default Twemoji;
