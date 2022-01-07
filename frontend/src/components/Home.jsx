b5cf4c64026ce9de7f4d7ca79d570ce2dadf7acd
SweetSofiMC <magallon.paramo@gmail.com>
2022-01-07 02:55:25 -0600

    Home with greeting animation

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,64 @@
+import './Home.scss'
+import data from './messages.js';
+import {useEffect, useState} from "react";
+
+const Home = () => {
+  let [currentText, setCurrentText] = useState('');
+  let [ticks, setTicks] = useState(0);
+  let [currentLine, setCurrentLine] = useState(0);
+  let [pauseTime, setPauseTime] = useState(0);
+  let [breakLine, setBreakLine] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() =>{
+      if(pauseTime > 0){
+        setPauseTime(pauseTime - 1);
+        return;
+      } else if(currentLine !== data.messages.length -1 && breakLine){
+        setCurrentText(data.messages[0].text);
+        setCurrentLine(currentLine + 1);
+        setTicks(0);
+        setBreakLine(false);
+        return;
+      }
+
+      let nextIncrease = 1;
+
+      if(ticks < data.messages[currentLine].text.length) {
+        let newValue = currentText;
+        if(data.messages[currentLine].text[ticks] === ' '){
+          newValue += ' ';
+          nextIncrease = 2;
+        }
+
+        setCurrentText(newValue + data.messages[currentLine].text[ticks + nextIncrease - 1]);
+        setTicks(ticks + nextIncrease);
+      } else {
+        if(currentLine !== 0){
+          setCurrentText(currentText + data.messages[currentLine].emoji);
+          if(currentLine === data.messages.length - 1){
+            console.log("Trying to clear interval" + interval)
+            clearTimeout(interval);
+          }
+          setPauseTime(5);
+        }
+        setBreakLine(true);
+      }
+
+    }, 70);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [ticks, currentText, currentLine, pauseTime, breakLine]);
+
+  return (
+    <div className="home-container">
+        <div className={"title-anim-container"}>
+          <h1 id={"title-anim"}>{currentText}<span className={"flickering-cursor"}>|</span></h1>
+        </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ frontend/src/components/messages.js
@@ -0,0 +1,14 @@
+let data = {
+  messages : [
+    {text :  "I'm"},
+    {text :  " a software developer", emoji :  "👩‍💻"},
+    {text :  " a GitHub Campus Expert", emoji : "🚩"},
+    {text :  " an MLH Fellowship Alumni", emoji: "🎓"},
+    {text :  " a Líder del Mañana", emoji: "✨"},
+    {text :  " mexican", emoji: "🇲🇽"},
+    {text :  " a daydreamer", emoji: "💭"},
+    {text :  " Sophie", emoji: "🌸"},
+  ]
+}
+
+export default data;
