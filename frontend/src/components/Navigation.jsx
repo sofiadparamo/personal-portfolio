8b59a1a90028c14e613b4fd75b754ff38974d79a
SweetSofiMC <sweetsofimc@gmail.com>
2022-01-08 18:08:57 -0600

    Fix styling for navbar dropdown

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -4,19 +4,19 @@ import Twemoji from "./Twemoji";
 
 const Navigation = () => {
     return(
-        <Navbar bg="light" expand="lg" className={"navbar-element"}>
-            <Container fluid>
-                <Navbar.Brand href="/" className={"branding"}>
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+            <Container>
+                <Navbar.Brand href="#home">
                     <Twemoji emoji={"🌸"} size={45}/>
                     <span className={"brand-name-text"}> SweetSofiMC</span>
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav" className={"justify-content-end"}>
-                    <Nav className="d-flex buttons-container">
-                        <Nav.Link href="#action1" className={"nav-buttons"}>Home</Nav.Link>
-                        <Nav.Link href="#action2" className={"nav-buttons"}>About</Nav.Link>
-                        <Nav.Link href="#action2" className={"nav-buttons"}>Work</Nav.Link>
-                        <Nav.Link href="#action2" className={"nav-buttons"}>Contact</Nav.Link>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav" className={"justify-content-end"}>
+                    <Nav>
+                        <Nav.Link href="/" className={"nav-buttons"}>Home</Nav.Link>
+                        <Nav.Link href="/about" className={"nav-buttons"}>About</Nav.Link>
+                        <Nav.Link href="/work" className={"nav-buttons"}>Work</Nav.Link>
+                        <Nav.Link href="/contact" className={"nav-buttons"}>Contact</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
